d9351578d451fca69463dfb82cc78786748dfb47
ahmed abu elregal <ahmedabuelregal@ahmeds-Mac.local>
2019-01-25 16:54:52 -0800

    Initial Commit

---
+++ script/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  script
+//
+//  Created by ahmed abu elregal on 1/25/19.
+//  Copyright © 2019 ahmed abu elregal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
