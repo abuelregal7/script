7cacb7a7c1e5c66f767899fc6b1e29cb1386da49
ahmed abu elregal <ahmedabuelregal@192.168.181.130>
2019-02-02 17:31:13 -0800

    26

--- script/ViewController.swift
+++ script/ViewController.swift
@@ -7,14 +7,67 @@
 //
 
 import UIKit
+import Speech
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , AVAudioPlayerDelegate {
+    
+    var recordButton: UIButton!
+    var audioPlayer: AVAudioPlayer!
 
+    @IBOutlet weak var transcriptionTextField: UITextView!
+    @IBOutlet weak var activitySpinner: UIActivityIndicatorView!
     override func viewDidLoad() {
+        activitySpinner.isHidden = true
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        player.stop()
+        activitySpinner.stopAnimating()
+        activitySpinner.isHidden = true
+    }
+    func requestSpeechAuth()
+    {
+        SFSpeechRecognizer.requestAuthorization{ authoStatus in
+            if authoStatus == SFSpeechRecognizerAuthorizationStatus.authorized{
+                if let path = Bundle.main.url(forResource: "audio", withExtension: "m4a"){
+                    do{
+                        let sound = try AVAudioPlayer(contentsOf: path)
+                        self.audioPlayer = sound
+                        self.audioPlayer.delegate = self
+                        sound.play()
+                        
+                    }catch{
+                        print("Error!")
+                        
+                    }
+                    let recognizer = SFSpeechRecognizer()
+                    let request = SFSpeechURLRecognitionRequest(url: path)
+                    recognizer?.recognitionTask(with: request) { (result,error) in
+                        if let error = error {
+                            print("There was an error: \(error)")
+                        } else {
+                            self.transcriptionTextField.text = result?.bestTranscription.formattedString
+                        }
+                        
+                    }
+                    
+                }
+            }
+            
+    }
 
 
+}
+    @IBAction func playBtnRecord(_ sender: UIButton) {
+        
+        
+        activitySpinner.isHidden = false
+        activitySpinner.startAnimating()
+        requestSpeechAuth()
+        
+    }
+    
 }
 
